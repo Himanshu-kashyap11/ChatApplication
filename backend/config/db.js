ef2d5324b57e59da51ed806afa81daddb86f0e46
Himanshu-kashyap11 <hk4071794@gmail.com>
2026-05-23 12:51:42 +0530

    fixed mongodb connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,7 +7,7 @@ dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
 const connectDb = async () =>{
     try {
-        await mongoose.connect(process.env.MONGODB_URL);
+       await mongoose.connect("mongodb+srv://Himanshu:12345@cluster0.mpcbict.mongodb.net/chatapp?retryWrites=true&w=majority");
         console.log("db connected")
     } catch (error) {
         console.log("db error", error.message);
