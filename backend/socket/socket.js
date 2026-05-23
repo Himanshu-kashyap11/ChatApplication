e677ca94190feca6a920e544d326006ca21a163f
Himanshu-kashyap11 <hk4071794@gmail.com>
2026-05-23 13:52:12 +0530

    remove localhost fallback

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -8,7 +8,9 @@ let app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: process.env.CLIENT_URL,
+    methods: ["GET", "POST"],
+    credentials: true,
   },
 });
 const userSocketMap = {};
